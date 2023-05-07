cb47e03907e6bca4a4334ded5c67f2d5fd633048
Rodion Groshev <rodion.groshev@gmail.com>
2023-05-07 20:04:07 +0300

    HW_Lesson_2_Retake

--- HW_Lesson_2/HW_Task_2.py
+++ HW_Lesson_2/HW_Task_2.py
@@ -1,10 +1,10 @@
 eleks_employees = ["John", "Sara", "Jessica", "Andrew", "Mike"]
 toshiba_employees = ["Peter", "Jack", "Helen", "Mike"]
 
-# First option
-# toshiba_employees.extend(eleks_employees)
-# print(toshiba_employees)
 
-# Second option
-toshiba_employees.append(eleks_employees)
+toshiba_employees.extend(eleks_employees)
+eleks_employees.clear()
 print(toshiba_employees)
+
+
+
--- HW_Lesson_2/HW_Task_4.py
+++ HW_Lesson_2/HW_Task_4.py
@@ -1,5 +1,5 @@
 omnivore_group = {"Harry Potter", "Jack Sparrow", "Sherlock Holmes", "James Bond"}
 vegetarian_group = {"James Bond", "Indiana Jones", "Rocky Balboa", "Han Solo"}
 
-vegetarian_group.update(omnivore_group)
-print(vegetarian_group)
+vegetables_herbs = omnivore_group.union(vegetarian_group)
+print(vegetables_herbs)
