351f649ad7bf34174ce3ab95772cb102ebd75ed4
Rodion Groshev <rodion.groshev@gmail.com>
2023-05-09 10:25:58 +0300

    Last change

--- HW_Lesson_2/HW_Task_5.py
+++ HW_Lesson_2/HW_Task_5.py
@@ -1,9 +1,7 @@
 vip_seats = ("Messi", "Ronaldo", "Zidane", "Totti", "Pele")
-common_room = ("Cafu", "Shevchenko", "Henry", None, None)
+common_room = ["Cafu", "Shevchenko", "Henry", None, None]
 
 print(vip_seats)
 print(common_room)
 
-res = vip_seats + common_room
 
-print(res)
