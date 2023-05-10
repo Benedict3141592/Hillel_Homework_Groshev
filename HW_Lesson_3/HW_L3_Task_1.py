ede604e86361f3aceb59c1d07ebc042bf2971b9d
Rodion Groshev <rodion.groshev@gmail.com>
2023-05-10 11:16:30 +0300

    First task retake

--- HW_Lesson_3/HW_L3_Task_1.py
+++ HW_Lesson_3/HW_L3_Task_1.py
@@ -2,11 +2,11 @@ lst_elements = [1, 2, 3, 4, 5, 6, 7, 8]
 lst_odd_index = []
 lst_even_index = []
 
-for numbers in lst_elements:
-    if lst_elements.index(numbers) % 2 == 0:
-        lst_odd_index.append((lst_elements.index(numbers), numbers))
+for index, number in enumerate(lst_elements):
+    if index % 2 == 0:
+        lst_odd_index.append((index, number))
     else:
-        lst_even_index.append((lst_elements.index(numbers), numbers))
+        lst_even_index.append((index, number))
 
 print(lst_odd_index)
 print(lst_even_index)
