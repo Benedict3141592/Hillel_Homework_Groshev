bd00a56462a1366dfe882f250f9348a397d99291
Rodion Groshev <rodion.groshev@gmail.com>
2023-05-15 15:18:01 +0200

    HW_Lesson_4_Task_2_Second_try

--- HW_Lesson_4/HW_L4_Task_2.py
+++ HW_Lesson_4/HW_L4_Task_2.py
@@ -1,17 +1,36 @@
-lst = ["FirstItem", "FriendsList", "MyTuple"]
+lst = ["FirstItem", "FriendsList", "MyTuple", "Anotherword", "word", "Another_dict"]
 res = []
 
+# First option
+# for word in lst:
+#     word = word.lower()
+#     if len(word) <= 4 or "_" in word:
+#         res.append(word)
+#     elif word[-4:] == "item" or word[-4:] == "list":
+#         word = list(word)
+#         word.insert(-4, "_")
+#         res.append("".join(word))
+#     elif word[-5:] == "tuple":
+#         word = list(word)
+#         word.insert(-5, "_")
+#         res.append("".join(word))
+#     else:
+#         word = list(word)
+#         word.insert(-4, "_")
+#         res.append("".join(word))
+
+# Second option (optimised)
 for word in lst:
     word = word.lower()
-    if word[-4:] == "item" or word[-4:] == "list":
-        word = list(word)
-        word.insert(-4, "_")
-        res.append("".join(word))
+    if len(word) <= 4 or "_" in word:
+        res.append(word)
     elif word[-5:] == "tuple":
         word = list(word)
         word.insert(-5, "_")
         res.append("".join(word))
     else:
-        res.append(word)
+        word = list(word)
+        word.insert(-4, "_")
+        res.append("".join(word))
 
 print(res)
